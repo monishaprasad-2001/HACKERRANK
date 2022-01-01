7a400f370cfaca3a9521f7a41dee6e679263d80c
monishaprasad-2001 <85404517+monishaprasad-2001@users.noreply.github.com>
2022-01-01 21:20:58 +0530

    Create designer-pdf.py

---
+++ designer-pdf.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    maxHeight=0
+    for i in range(len(word)):
+        if(h[ord(word[i])-97] > maxHeight):
+            maxHeight=h[ord(word[i])-97]
+    return(len(word)*1*maxHeight)
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
